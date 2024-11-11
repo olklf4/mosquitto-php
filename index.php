e3d8d8258a9e09fe9e74a7b7f067bab53d15a1f5
olklf4 <6MEg6B+olklf4@gmail.com>
2024-11-11 22:32:05 +0700

    Include Bootstrap compiled CSS & JS and add an index page
    Exclude unnecessary Bootstrap files
    The index page shows the website in construction

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Bảng Điều Khiển</title>
+		<link href="/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65">
+	</head>
+	<body>
+		<div class="container-xxl">
+			<div class="alert alert-warning alert-dismissible fade show" role="alert">
+				Xây dựng bởi T.M.T.
+				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Đóng"></button>
+			</div>
+			<div class="alert alert-danger d-flex align-items-center" role="alert">
+				<svg class="bi flex-shrink-0 me-2" role="img" aria-label="Lưu ý:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+				<div>Trang đang trong quá trình bảo trì!</div>
+			</div>
+		</div>
+		<script src="/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"></script>
+	</body>
+</html>
