4ebb796ab4fc12dc7205c644a4b7573082db9ce6
olklf4 <6MEg6B+olklf4@gmail.com>
2024-11-12 18:57:41 +0700

    Include Bootstrap icons and modify the index page
    Exclude Bootstrap SVG files
    Index: fix source integrities and minor styles

--- index.php
+++ index.php
@@ -1,22 +1,38 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Bảng Điều Khiển</title>
-		<link href="/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65">
-	</head>
-	<body>
-		<div class="container-xxl">
-			<div class="alert alert-warning alert-dismissible fade show" role="alert">
-				Xây dựng bởi T.M.T.
-				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Đóng"></button>
-			</div>
-			<div class="alert alert-danger d-flex align-items-center" role="alert">
-				<svg class="bi flex-shrink-0 me-2" role="img" aria-label="Lưu ý:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-				<div>Trang đang trong quá trình bảo trì!</div>
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Bảng Điều Khiển</title>
+	<link href="/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA" />
+	<link href="/css/bootstrap-icons.min.css" rel="stylesheet"
+		integrity="sha512-dPXYcDub/aeb08c63jRq/k6GaKccl256JQy/AnOq7CAnEZ9FzSL9wSbcZkMp4R26vBsMLFYH4kQ67/bbV8XaCQ" />
+</head>
+
+<body>
+	<div class="container-xxl mt-3">
+		<div class="row">
+			<div class="col">
+				<div class="alert alert-warning alert-dismissible fade show" role="alert">
+					Xây dựng bởi T.M.T.
+					<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Đóng"></button>
+				</div>
 			</div>
 		</div>
-		<script src="/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"></script>
-	</body>
+		<div class="row">
+			<div class="col">
+				<div class="alert alert-danger d-flex align-items-center flex-shrink-0" role="alert">
+					<i class="bi bi-exclamation-triangle-fill me-2"></i>
+					<div>Trang đang trong quá trình bảo trì!</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="/js/bootstrap.bundle.min.js"
+		integrity="sha512-i9cEfJwUwViEPFKdC1enz4ZRGBj8YQo6QByFTF92YXHi7waCqyexvRD75S5NVTsSiTv7rKWqG9Y5eFxmRsOn0A"></script>
+</body>
+
 </html>
